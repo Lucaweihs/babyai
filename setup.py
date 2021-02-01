0b450eeb3a2dc7116c67900d51391986bdbb84cd
Luca Weihs <lucaw@allenai.org>
2021-02-01 15:38:35 -0800

    Adding missing find_packages.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='babyai',
     version='0.1.0',
     license='BSD 3-clause',
     keywords='memory, environment, agent, rl, openaigym, openai-gym, gym',
-    packages=['babyai', 'babyai.*'],
+    packages=find_packages(include=['babyai', 'babyai.*']),
     install_requires=[
         "gym>=0.9.6",
         "numpy>=1.15",
